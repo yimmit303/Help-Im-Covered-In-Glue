b78212fb3700f2aa3bcef7d98e5b0bb0a206c1c0
yimmit303 <timsteiner303@gmail.com>
2021-06-13 10:55:47 -0400

    Conveyors push in the right direction

--- Level/ConveyorBelt/ConveyorBelt.gd
+++ Level/ConveyorBelt/ConveyorBelt.gd
@@ -1,13 +1,19 @@
 extends StaticBody
 
 var bodys_on = []
+var force = Vector3(0,0,10)
+
+func _ready():
+	force = force.rotated(Vector3(0, 1, 0), deg2rad(self.rotation_degrees.y))
+	
 
 func _process(delta):
 	for body in bodys_on:
-		body.add_force(Vector3(0,0,10), Vector3(0,0,0))
+		body.add_force(force, Vector3(0,0,0))
 
 func _on_Conveyor_Area_body_entered(body):
 	if body is RigidBody:
+		print(rotation_degrees)
 		bodys_on.append(body)
 
 func _on_Conveyor_Area_body_exited(body):
