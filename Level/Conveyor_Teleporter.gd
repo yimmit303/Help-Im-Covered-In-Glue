47e53a37fa46167693c866bab0fd07a42e18d43e
yimmit303 <timsteiner303@gmail.com>
2021-06-13 13:32:15 -0400

    Slime pit on room 3

--- Level/Conveyor_Teleporter.gd
+++ Level/Conveyor_Teleporter.gd
@@ -8,7 +8,6 @@ func _ready():
 
 func entered(body):
 	if body is RigidBody and not body.is_in_group("body_part"):
-		print("body entered")
 		body.mode = 3
 		body.set_translation(body.get_translation() + offset) 
 		body.mode = 0
--- Level/Teleporter.gd
+++ Level/Teleporter.gd
@@ -3,8 +3,6 @@ extends Area
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("body_entered", self, "entered")
-	pass # Replace with function body.
-
 
 func entered(body):
 	if body.is_in_group("body_part"):
