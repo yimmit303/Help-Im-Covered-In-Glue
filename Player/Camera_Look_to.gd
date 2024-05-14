35a942bbe9eeea1490d5e8a314b9acbc6f6661f9
yimmit303 <timsteiner303@gmail.com>
2024-05-13 20:18:57 -0400

    Updated the camera to be able to look up and down

--- Player/Camera_Look_to.gd
+++ Player/Camera_Look_to.gd
@@ -12,7 +12,13 @@ func _process(delta):
 
 func _input(event):
 	if event is InputEventMouseMotion:
+		event.relative *= 0.521
 		self.rotation_degrees.y -= event.relative.x
+		if event.relative.y < 0 and self.rotation_degrees.x > -30:
+			self.rotation_degrees.x += event.relative.y
+		if event.relative.y > 0 and self.rotation_degrees.x < 50:
+			self.rotation_degrees.x += event.relative.y
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
